1a3a054250cd779ce332b0ff5f12282c9090f370
percymittu <47188139+percymittu@users.noreply.github.com>
2021-01-18 12:24:32 -0500

    Update functions.js
    Generated calculateTotal() which returns the product of price and quantity as total.Declared an outputCartRow() which returns the calculated total to the markup page.

--- ch08-project1-start-master/js/functions.js
+++ ch08-project1-start-master/js/functions.js
@@ -1,15 +1,12 @@
 /* define functions here */
 function calculateTotal(quantity,price)
 {
-return quantity * price;
+return quantity*price;
+}/* define functions here */
+var st=0;
+function outputCartRow(file,title,quantity,price,total){
+
+document.write("<tr> <td><img src=images/"+file+">  <td>"+title+"</td></td> <td>"+quantity+"</td> <td>$"+price.toFixed(2)+"</td> <td>$"+calculateTotal(quantity,price).toFixed(2)+"</td> </tr>");
+st=st+calculateTotal(quantity,price);
+return st;
 }
-function outputCartRow(file,title,quantity,price,total)
-{
-total=calculateTotal(quantity[i],price[i]);
-document.write("<tr>");
-document.write("<td>"+title[i]+"</td>");
-document.write("<td>"+quantity[i]+"</td>");
-document.write("<td>"+price[i].toFixed(2)+"</td>");
-document.write("</tr>");
-}
-        
