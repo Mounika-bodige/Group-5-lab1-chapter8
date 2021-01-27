6eda39cfdc03c4a1a90e99a9e8c923b65f34f4e4
percymittu <47188139+percymittu@users.noreply.github.com>
2021-01-27 18:27:21 -0500

    Add files via upload
    Data.js

---
+++ ch08-project2-start-master/Data.js
@@ -0,0 +1,23 @@
+var countries=
+[
+ {"country":"canada", //first country
+  "continent":"North America",
+ "cities":["Toronto","Montreal","Calgery","Thunder Bay"],
+ "images":["canada1.jpg","canada2.jpg","canada3.jpg"]},
+
+ {"country":"United States", //second country
+  "continent":"North America",
+ "cities":["Boston","Chicago","New York","Seattle","Washington"],
+ "images":["us1.jpg","us2.jpg"]},
+
+ {"country":"Italy",  //third country
+ "continent":"Europe",
+ "cities":["Florence","Milan","Naples","Rome"],
+ "images":["italy4.jpg","italy5.jpg","italy6.jpg"]},
+
+  {"country":"Spain", //fourth country
+  "continent":"Europe",
+  "cities":["Almeria","Barcelona","Madrid"],
+  "images":["spain1.jpg","spain2.jpg"]},
+]
+
